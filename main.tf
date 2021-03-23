6f42af62f43c750e3573b08d9bdf69bf1da54fa7
nikhilpatne <66066035+nikhilpatne@users.noreply.github.com>
2021-03-23 16:25:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+client_id       =   var.client_id
+client_secret   =   var.client_secret
+subscription_id =   var.subscription_id
+tenant_id       =   var.tenant_id
+  
 }
 
 
