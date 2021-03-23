7c7f9e0f1961e1243244a258033868bcb09e6dc7
nikhilpatne <66066035+nikhilpatne@users.noreply.github.com>
2021-03-23 12:32:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-subscription_id: d8545a7b-b1b2-4468-a93d-5203ee3b0bb3
-tenant_id: e14e73eb-5251-4388-8d67-8f9f2e2d5a46
-client_id: 70354295-3c25-407b-a84a-025e23e4c605
-client_secret:  mKx90_1iuE-ro-yOepQXmjC3F_~Ul6X8Wx
+subscription_id =  d8545a7b-b1b2-4468-a93d-5203ee3b0bb3
+tenant_id =  e14e73eb-5251-4388-8d67-8f9f2e2d5a46
+client_id =  70354295-3c25-407b-a84a-025e23e4c605
+client_secret =  mKx90_1iuE-ro-yOepQXmjC3F_~Ul6X8Wx
 }
 
 
