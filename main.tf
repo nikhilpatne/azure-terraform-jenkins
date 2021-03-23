17b27a08340d42f17be6aa3a4d49de228d9ab2a2
nikhilpatne <66066035+nikhilpatne@users.noreply.github.com>
2021-03-23 16:38:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,25 @@ resource "azurerm_resource_group" "example" {
   name     = "nikhilrgg"
   location = "eastus"
 }
+
+
+
+variable "client_id" {
+    description =   "Client ID (APP ID) of the application"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Client Secret (Password) of the application"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "Subscription ID"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "Tenant ID"
+    type        =   string
+}
